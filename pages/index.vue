18acc07591038649f8c3a02ba8cf943383ebf6c8
thanhdat <thanhdatwg@gmail.com>
2020-12-11 14:19:41 +0700

    create HomePage

--- pages/index.vue
+++ pages/index.vue
@@ -1,41 +1,73 @@
 <template>
-  <v-card class="overflow-hidden">
-    <v-app-bar
-      absolute
-      color="white"
-      elevate-on-scroll
-      scroll-target="#scrolling-techniques-7"
-    >
-      <v-row class="d-flex" align="center">
-        <v-col cols="3"><v-toolbar-title>All Questions</v-toolbar-title></v-col>
-        <v-col cols="1" class="ml-2">
-          <v-btn icon>
-            <v-icon>mdi-magnify</v-icon>
-          </v-btn></v-col
+  <v-card>
+    <v-toolbar color="cyan" dark flat>
+      <v-toolbar-title>Top Questions</v-toolbar-title>
+      <v-spacer></v-spacer>
+      <v-text-field label="Search" outlined dense class="mt-5">
+        <v-icon>mdi-magnify</v-icon></v-text-field
+      >
+      <v-btn icon>
+        <v-icon>mdi-magnify</v-icon>
+      </v-btn>
+      <v-spacer></v-spacer>
+
+      <v-btn color="white" outlined to="/create">
+        Ask Question
+      </v-btn>
+
+      <template v-slot:extension>
+        <v-tabs v-model="tab" align-with-title>
+          <v-tabs-slider color="orange lighten-2"></v-tabs-slider>
+
+          <v-tab v-for="item in items" :key="item">
+            {{ item }}
+          </v-tab>
+        </v-tabs>
+      </template>
+    </v-toolbar>
+
+    <v-tabs-items v-model="tab">
+      <v-tab-item v-for="item in items" :key="item">
+        <v-card
+          v-for="(infoQuestion, index) in infoQuestions"
+          :key="index"
+          flat
         >
-        <v-col cols="5" class="justify-start mt-6 ml-n10"
-          ><v-text-field label="Search..." outlined dense></v-text-field
-        ></v-col>
-
-        <v-spacer></v-spacer>
-
-        <v-col cols="2">
-          <v-btn color="primary" to="/create">
-            Ask Question
-          </v-btn></v-col
-        >
-      </v-row>
-    </v-app-bar>
-
-    <v-sheet
-      id="scrolling-techniques-7"
-      class="overflow-y-auto"
-      max-height="600"
-    >
-      <v-container style="height: 1500px;">
-        <v-card-text><div>dslakm</div></v-card-text>
-      </v-container>
-    </v-sheet>
+          <v-card-text>
+            <v-row no-gutters class="d-flex justify-center" align="center">
+              <v-col cols="1" align="center">
+                <div>{{ infoQuestion.vote }}</div>
+                <div>vote</div>
+              </v-col>
+              <v-col cols="1" align="center">
+                <div>{{ infoQuestion.answer }}</div>
+                <div>answer</div>
+              </v-col>
+              <v-col cols="1" align="center" style="border: 1px solid green">
+                <div>{{ infoQuestion.views }}</div>
+                <div>views</div>
+              </v-col>
+              <v-spacer></v-spacer>
+              <v-col cols="8">
+                <div
+                  class="text-body-2 font-weight-medium"
+                  style="color:#0064BD"
+                >
+                  {{ infoQuestion.title }}
+                </div>
+              </v-col>
+            </v-row>
+            <v-row no-gutters>
+              <v-col cols="12" class="d-flex justify-end"
+                >Asked by {{ infoQuestion.creator }}
+                {{ infoQuestion.timeCreate }}</v-col
+              >
+            </v-row>
+          </v-card-text>
+          <v-divider></v-divider>
+        </v-card>
+      </v-tab-item>
+    </v-tabs-items>
   </v-card>
 </template>
 
@@ -44,6 +76,106 @@
 // import VuetifyLogo from "~/components/VuetifyLogo.vue";
 
 export default {
+  data() {
+    return {
+      tab: null,
+      items: ["All", "Interesting", "Bountied", "Hot", "Week"],
+      text:
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+      infoQuestions: [
+        {
+          title:
+            "Is there a way to customize the input element for an Faktor-IPS extension property?",
+          vote: 3,
+          answer: 2,
+          views: 100,
+          creator: "Võ Quang Thành Đạt",
+          timeCreate: "2 days ago"
+        },
+        {
+          title:
+            "Getting analysis exception; parquet data source does not support null data type in pyspark",
+          vote: 10,
+          answer: 5,
+          views: 20,
+          creator: "Nguyễn Khắc Thắng",
+          timeCreate: "5 days ago"
+        },
+        {
+          title:
+            "I'm writing a code to take a user input and provide its documentation in python. But the string in the user input is used with quotes in python",
+          vote: 4,
+          answer: 5,
+          views: 17,
+          creator: "Vũ Xuân Chiến",
+          timeCreate: "2 days ago"
+        },
+        {
+          title:
+            "view show can we access the datatable individual column searched value to controller(C# .net Mvc) while using the server side processing?",
+          vote: 3,
+          answer: 2,
+          views: 100,
+          creator: "Võ Quang Thành Đạt",
+          timeCreate: "8 days ago"
+        },
+        {
+          title:
+            "JS : How to modify options we receive in event Handler and pass it further to original handler function?",
+          vote: 0,
+          answer: 0,
+          views: 10,
+          creator: "Võ Quang Thành Đạt",
+          timeCreate: "9 days ago"
+        },
+        {
+          title:
+            "Python programs hangs after aycnio exception due to Pyppeteer unexpectedly closing",
+          vote: 5,
+          answer: 4,
+          views: 30,
+          creator: "Võ Quang Thành Đạt",
+          timeCreate: "2 days ago"
+        },
+        {
+          title:
+            "Python programs hangs after aycnio exception due to Pyppeteer unexpectedly closing",
+          vote: 5,
+          answer: 4,
+          views: 30,
+          creator: "Võ Quang Thành Đạt",
+          timeCreate: "2 days ago"
+        },
+        {
+          title:
+            "Python programs hangs after aycnio exception due to Pyppeteer unexpectedly closing",
+          vote: 5,
+          answer: 4,
+          views: 30,
+          creator: "Võ Quang Thành Đạt",
+          timeCreate: "2 days ago"
+        },
+        {
+          title:
+            "Python programs hangs after aycnio exception due to Pyppeteer unexpectedly closing",
+          vote: 5,
+          answer: 4,
+          views: 30,
+          creator: "Võ Quang Thành Đạt",
+          timeCreate: "2 days ago"
+        },
+        {
+          title:
+            "Python programs hangs after aycnio exception due to Pyppeteer unexpectedly closing",
+          vote: 5,
+          answer: 4,
+          views: 30,
+          creator: "Võ Quang Thành Đạt",
+          timeCreate: "2 days ago"
+        }
+      ]
+    };
+  }
   // components: {
   //   Logo,
   //   VuetifyLogo
