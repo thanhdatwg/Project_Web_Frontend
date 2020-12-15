03b65afbbc176c4f601a97bbfce49e99a860e157
VuXuanChien <vuxuanchien249@gmail.com>
2020-12-15 15:46:01 +0700

    create Profilepage

--- pages/profile/index.vue
+++ pages/profile/index.vue
@@ -1,29 +1,49 @@
 <template>
-  <v-card class="mx-auto" max-width="434" tile>
-    <v-img
-      height="100%"
-      src="https://cdn.vuetifyjs.com/images/cards/server-room.jpg"
-    >
-      <v-row align="end" class="fill-height">
-        <v-col align-self="start" class="pa-0" cols="12">
-          <v-avatar class="profile" color="grey" size="164" tile>
+  <v-card class="mx-auto" tile>
+    <v-card-text>
+      <v-row class="d-flex justify-center" align="center">
+        <v-col cols="4" align="center">
+          <h1 class="black--text font-weight-medium">
+            Edit Profile
+          </h1>
+          <v-avatar class="profile mt-2" color="grey" size="164">
             <v-img
               src="https://cdn.vuetifyjs.com/images/profiles/marcus.jpg"
             ></v-img>
           </v-avatar>
+          <div class="d-flex justify-center">
+            <input type="file" @change="onFileChange" />
+          </div>
         </v-col>
-        <v-col class="py-0">
-          <v-list-item color="rgba(0, 0, 0, .4)" dark>
-            <v-list-item-content>
-              <v-list-item-title class="title">
-                Marcus Obrien
-              </v-list-item-title>
-              <v-list-item-subtitle>Network Engineer</v-list-item-subtitle>
-            </v-list-item-content>
-          </v-list-item>
+        <v-spacer></v-spacer>
+        <v-col cols="8">
+          <v-row no-gutters>
+            <v-text-field label="First Name" outlined> </v-text-field>
+          </v-row>
+          <v-row no-gutters>
+            <v-text-field label="Last Name" outlined> </v-text-field>
+          </v-row>
+          <v-row no-gutters>
+            <v-text-field label="Email" outlined> </v-text-field>
+          </v-row>
+          <v-row no-gutters>
+            <v-text-field label="Address" outlined> </v-text-field>
+          </v-row>
+          <v-row no-gutters class="d-flex justify-center">
+            <v-col class="col-auto">
+              <v-btn color="primary" class="mr-1">
+                Save Change
+              </v-btn>
+            </v-col>
+            <v-col class="col-auto">
+              <v-btn color="white" class="ml-1">
+                Cancel
+              </v-btn>
+            </v-col>
+          </v-row>
         </v-col>
       </v-row>
-    </v-img>
+    </v-card-text>
   </v-card>
 </template>
 
